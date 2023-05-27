0bffc5e52efedfacb78877dce9d5174b9b8f8484
krst221 <krst221@gmail.com>
2023-05-27 21:15:57 +0200

    refactor y traducciones

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,48 +1,52 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all.with_attached_photo
   end
+
   def show
-    @product = Product.find(params[:id])
+    product
   end
+
   def new
     @product = Product.new
   end
+
   def create
     @product = Product.new(product_params)
 
     if @product.save
-      redirect_to products_path, notice: 'Tu producto se ha creado correctamente'
+      redirect_to products_path, notice: t('.created')
     else
       render :new, status: :unprocessable_entity 
     end
   end
 
   def edit
-    @product = Product.find(params[:id])
+    product
   end
 
   def update
-    @product = Product.find(params[:id])
-
-    if @product.update(product_params)
-      redirect_to products_path, notice: 'Tu producto se ha actualizado correctamente'
+    if product.update(product_params)
+      redirect_to products_path, notice: t('.updated')
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
+    product.destroy
 
-    redirect_to products_path, notice: 'Tu producto se ha eliminado correctamente'
+    redirect_to products_path, notice: t('.destroyed')
   end
 
   private
 
   def product_params
     params.require(:product).permit(:title, :description, :price, :photo)
+  end
+
+  def product
+    @product = Product.find(params[:id])
   end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module Saleschris
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    #available languages
+    config.i18n.available_locales = [:en, :es]
+
+    #default language
+    config.i18n.default_locale = :es
   end
 end
