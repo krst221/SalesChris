e90a4f5b8621648f89850bbdda63d5c33fa86e20
krst221 <krst221@gmail.com>
2023-05-28 17:17:30 +0200

    infinite scrolling

--- Gemfile
+++ Gemfile
@@ -45,6 +45,9 @@ gem "bootsnap", require: false
 # Postgresql fulltextsearch
 gem "pg_search", "~> 2.3"
 
+# Pagination
+gem "pagy", "~> 6.0"
+
 # Use Sass to process CSS
 # gem "sassc-rails"
 
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -5,22 +5,22 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     get products_path 
 
     assert_response :success
-    assert_select '.product', 3
-    assert_select '.category', 3
+    assert_select '.product', 12
+    assert_select '.category', 9
   end
 
   test 'render a list of products filtered by category' do
     get products_path(category_id: categories(:computers).id)
 
     assert_response :success
-    assert_select '.product', 1
+    assert_select '.product', 5
   end
 
   test 'render a list of products filtered by min_price and max_price' do
     get products_path(min_price: 160, max_price: 200)
 
     assert_response :success
-    assert_select '.product', 1
+    assert_select '.product', 3
     assert_select 'h2', 'Nintendo Switch'
   end
 
@@ -32,13 +32,29 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_select 'h2', 'Nintendo Switch'
   end
 
+  test 'sort products by most expensive prices first' do
+    get products_path(order_by: 'expensive')
+
+    assert_response :success
+    assert_select '.product', 12
+    assert_select '.products .product:first-child h2', 'Seat Panda 1990'
+  end
+
+  test 'sort products by cheapest prices first' do
+    get products_path(order_by: 'cheapest')
+
+    assert_response :success
+    assert_select '.product', 12
+    assert_select '.products .product:first-child h2', 'El Hobbit'
+  end
+
   test 'render a detailed product page' do
     get product_path(products(:switch))
 
     assert_response :success
     assert_select '.title', 'Nintendo Switch'
-    assert_select '.description', 'Versión OLED, en buen estado'
-    assert_select '.price', '200€'
+    assert_select '.description', 'Le falla el lector de tarjeta SD'
+    assert_select '.price', '195€'
   end
 
   test 'render a new product form' do
